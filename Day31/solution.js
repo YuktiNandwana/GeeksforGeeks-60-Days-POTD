d6ce1f89f1243a31150fdd135def928862ae9751
Yukti Nandwana <nandwanayukti@gmail.com>
2026-03-20 00:44:08 +0530

    Refactor findPreSuc to return ArrayList of nodes
    Refactor findPreSuc method to return ArrayList of predecessor and successor instead of using arrays.

--- Day31/solution.js
+++ Day31/solution.js
@@ -1,25 +1,37 @@
+import java.util.ArrayList;
+
 class Solution {
-    public static void findPreSuc(Node root, Node[] pre, Node[] suc, int key) {
-        // Step 1: Find Predecessor
+    public ArrayList<Node> findPreSuc(Node root, int key) {
+        Node pre = null;
+        Node suc = null;
+        
+        // Step 1: Find Predecessor (just smaller than key)
         Node curr = root;
         while (curr != null) {
             if (curr.data < key) {
-                pre[0] = curr; // Save as potential predecessor
-                curr = curr.right; // Move right to find a closer, larger value
+                pre = curr; // Save as potential predecessor
+                curr = curr.right; // Move right to get closer
             } else {
-                curr = curr.left; // Value is too big, move left
+                curr = curr.left; // Too big or equal, move left
             }
         }
         
-        // Step 2: Find Successor
+        // Step 2: Find Successor (just greater than key)
         curr = root;
         while (curr != null) {
             if (curr.data > key) {
-                suc[0] = curr; // Save as potential successor
-                curr = curr.left; // Move left to find a closer, smaller value
+                suc = curr; // Save as potential successor
+                curr = curr.left; // Move left to get closer
             } else {
-                curr = curr.right; // Value is too small, move right
+                curr = curr.right; // Too small or equal, move right
             }
         }
+        
+        // Step 3: List mein daal kar return kar do
+        ArrayList<Node> result = new ArrayList<>();
+        result.add(pre);
+        result.add(suc);
+        
+        return result;
     }
 }
